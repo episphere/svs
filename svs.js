b567f2abcdb4cf2f03be490122cf4035e31c3d0e
Praful Bhawsar <prtsh32@gmail.com>
2024-09-03 09:01:00 -0400

    Moving SVS demo to GeoTIFFTileSource. NDPI images might not work.

--- svs.js
+++ svs.js
@@ -24,7 +24,7 @@ svs.readJSON = async (url = 'HALO All Sherlock IDs 07282020_HP0493-001-007.json'
 }
 
 svs.gcsBasePath = "https://storage.googleapis.com/imagebox_test"
-svs.serverBasePath = "https://imageboxv2-oxxe7c4jbq-uc.a.run.app/iiif"
+// svs.serverBasePath = "https://imageboxv2-oxxe7c4jbq-uc.a.run.app/iiif"
 // svs.serverBasePath = "http://localhost:8080/iiif"
 
 svs.loadHashParams = async () => {
@@ -131,23 +131,23 @@ svs.loadImage = async (urlInGCP) => {
   // 	return
   // }
   urlInGCP = urlInGCP.replace(/\s/g, "_")
-  const format = urlInGCP.endsWith(".ndpi") ? "ndpi" : "svs"
-  const p = `${svs.serverBasePath}/?iiif=${urlInGCP}`;
-  const infoURL = `${p}/info.json`
-  let imageInfo
-  try {
-    imageInfo = await (await fetch(infoURL)).json()
-  } catch (e) {
-    alert("An error occurred retrieving the image information. Please try again later.")
-    // window.history.back()
-    // setTimeout(() => {
-    // 	document.getElementById("imageSelectName").value = hashParams.imageTag
-    // 	document.getElementById("imageSelectId").value = hashParams.imageNslcId
-    // }, 1000)
-    document.getElementById("loadingText").style.display = "none"
-    return
-  }
-  console.log("image Info : ", imageInfo)
+  // const format = urlInGCP.endsWith(".ndpi") ? "ndpi" : "svs"
+  // const p = `${svs.serverBasePath}/?iiif=${urlInGCP}`;
+  // const infoURL = `${p}/info.json`
+  // let imageInfo
+  // try {
+  //   imageInfo = await (await fetch(infoURL)).json()
+  // } catch (e) {
+  //   alert("An error occurred retrieving the image information. Please try again later.")
+  //   // window.history.back()
+  //   // setTimeout(() => {
+  //   // 	document.getElementById("imageSelectName").value = hashParams.imageTag
+  //   // 	document.getElementById("imageSelectId").value = hashParams.imageNslcId
+  //   // }, 1000)
+  //   document.getElementById("loadingText").style.display = "none"
+  //   return
+  // }
+  // console.log("image Info : ", imageInfo)
 
   // const infoTable = document.getElementById("infoTable")
   // infoTable.innerHTML = ""
@@ -168,25 +168,17 @@ svs.loadImage = async (urlInGCP) => {
   // })
 
   document.getElementById("openseadragon1").innerHTML = ""
+  const tileSources = await OpenSeadragon.GeoTIFFTileSource.getAllTileSources(urlInGCP, { logLatency: false, cache: true, slideOnly: true })
+  console.log(tileSources)
   const viewer1 = OpenSeadragon({
     id: "openseadragon1",
-    preserveViewport: true,
     visibilityRatio: 1,
-    minZoomLevel: 1,
-    defaultZoomLevel: 1,
-    prefixUrl: "/openseadragon/images/",
-    tileSources: {
-      "@context": imageInfo["@context"],
-      "@id": p,
-      "height": parseInt(imageInfo.height),
-      "width": parseInt(imageInfo.width),
-      "profile": ["http://iiif.io/api/image/2/level2.json"],
-      "protocol": "http://iiif.io/api/image",
-      "tiles": [{
-        "scaleFactors": [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576],
-        "width": 256
-      }]
-    }
+    minZoomImageRatio: 1,
+    prefixUrl: "https://episphere.github.io/svs/openseadragon/images/",
+    imageLoaderLimit: 5,
+    timeout: 1000 * 1000,
+    crossOriginPolicy: "Anonymous",
+    tileSources: await OpenSeadragon.GeoTIFFTileSource.getAllTileSources(urlInGCP, { logLatency: false, cache: true, slideOnly: true })
   });
   setTimeout(() => document.getElementById("loadingText").style.display = "none", 5000)
 }
